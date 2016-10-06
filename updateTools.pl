9a5c7877b966d95d91c20c12ee762ba53274f788
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-10-06 19:41:28 -0400

    updateTools.pl

---
+++ updateTools.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my @repos=qw(
+    https://github.com/jonbri/killer|killer
+    https://github.com/jonbri/whereami|whereami
+);
+
+sub doHelp(;$) {
+    my ($errMsg) = @_;
+    if ($errMsg) {
+        print "error: $errMsg\n";
+    }
+    print STDERR <<'HELP';
+----------------------------------------------------------------
+updateTools
+----------------------------------------------------------------
+
+Basic Usage:
+  >> ./updateTools
+
+HELP
+exit 1;
+};
+
+sub runCommand($) {
+    my ($command)=@_;
+    open CMD,"$command |";
+    my @output=<CMD>;
+    close CMD;
+    return @output;
+}
+
+MAIN: {
+    my $platform=$^O;
+    my $home=$ENV{HOME};
+
+    my $toolsDir;
+    my $homeBinDir;
+
+    if($platform eq 'linux') {
+        $toolsDir="$home/tools";
+        $homeBinDir="$home/bin";
+    } elsif($platform eq 'msys') {
+        $toolsDir="$home/tools";
+        $homeBinDir="$home/bin";
+    } elsif($platform eq 'MSWin32') {
+        $toolsDir='C:\Users\jonbri\tools';
+        $homeBinDir='C:\Users\jonbri\bin';
+    } else {
+        print "unknown platform\n";
+        exit 1;
+    }
+
+    runCommand("mkdir -p $toolsDir");
+    runCommand("mkdir -p $homeBinDir");
+    runCommand("curl --silent https://raw.githubusercontent.com/jonbri/copybuddy/master/copybuddy > $homeBinDir/copybuddy");
+    runCommand("chmod +x $homeBinDir/copybuddy");
+
+    my ($repo,$folderName);
+    foreach(@repos) {
+        ($repo,$folderName)=split("\Q|",$_,2);
+
+        my $fullPath="$toolsDir/$folderName";
+        if (-d "$fullPath") {
+            runCommand("git -C $fullPath stash -u");
+            runCommand("git -C $fullPath pull origin master");
+        } else {
+            runCommand("git clone $repo $fullPath");
+        }
+
+        if (-e "$fullPath/.cb") {
+            runCommand("copybuddy $fullPath/.cb");
+        }
+    }
+
+    exit;
+}
