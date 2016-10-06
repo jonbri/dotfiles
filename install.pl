84f3e5b1a036b6dbe69eb30ca1fedb763b3c47d6
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-10-06 11:33:58 -0400

    install script

---
+++ install.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Cwd 'cwd';
+
+my $home=$ENV{HOME};
+my $cwd = cwd();
+
+sub runCommand($) {
+    my ($command) = @_;
+    open CMD, "$command |" || die "Unable to run $command: $!";
+    my @output = <CMD>;
+    close CMD;
+    return @output;
+}
+
+# read dot files directory
+opendir(DH, "$cwd");
+my @files = readdir(DH);
+closedir(DH);
+
+foreach (@files) {
+    # skip . and .. and others
+    next if($_ =~ /^\.$/);
+    next if($_ =~ /^\.\.$/);
+    next if($_ =~ /^\.git$/);
+    next if($_ =~ /^\.gitignore$/);
+    next if($_ =~ /^install.pl$/);
+
+    print join('', runCommand("ln -sfv $cwd/$_ $home"));
+}
