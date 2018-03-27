6c29bd4a18d47c9cfa005ac18658e27f66fdd4df
Jonathan Brink <jonathandavidbrink@gmail.com>
2018-03-27 11:47:30 -0400

    zargs

--- bin/updateVimPlugins.pl
+++ bin/updateVimPlugins.pl
@@ -35,6 +35,7 @@ my @repos=qw(
     https://github.com/jonbri/vim-pretty.git|jonbri-pretty
     https://github.com/jonbri/vim-spaced.git|jonbri-spaced
     https://github.com/jonbri/vim-jessdoc.git|jonbri-jessdoc
+    https://github.com/jonbri/vim-zargs.git|jonbri-zargs
     https://github.com/goldfeld/ctrlr.vim.git|goldfeld-ctrl
 );
 
