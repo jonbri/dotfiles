3fcd1cdea788fa99eb59c84866181e8c635fd8ad
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-05-26 15:46:31 -0400

    more neovim lua conversion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,7 +47,9 @@ require("oil").setup({
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
 
 vim.g.mapleader = " "
-
+-- TODO: test these
+vim.opt.pastetoggle = "<F3>"
+vim.opt.showtabline = 2
 vim.g.flash_winswitch=1
 
 vim.api.nvim_set_keymap(
@@ -71,8 +73,13 @@ vim.api.nvim_set_keymap(
   { noremap = true }
 )
 
--- TODO: test this
-vim.opt.pastetoggle = "<F3>"
+-- TODO: use folke/zen-mode.nvim and replace Goyo
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>g",
+--   ":Goyo",
+--   { noremap = true }
+-- )
 
 function GetCC()
   local defaultCC = "80"
@@ -88,138 +95,9 @@ vim.keymap.set(
   GetCC, { noremap = true }
 )
 
---[[
+-- TODO
+-- " quick-fix window always spans windows at bottom
+-- " https://stackoverflow.com/a/47077341/2295034
+-- au FileType qf wincmd J
 
-""""""""""""""""""""""""""""""""""""""""""
-" brace-jump
-function! BraceJump()
-  normal 0
-  execute "normal! /\v[\[{]<cr>"
-  normal 0$%$
-  execute "normal! /\\v\\S\<cr>"
-endfunction
-" nnoremap g} :call BraceJump()<CR>
-nnoremap <silent> <Plug>JonbriBraceJump :call BraceJump()<cr>
-\:call repeat#set("\<Plug>JonbriBraceJump")<CR>
-nmap g} <Plug>JonbriBraceJump
-""""""""""""""""""""""""""""""""""""""""""
-
-
-""""""""""""""""""""""""""""""""""""""""""
-" search matching
-function! GotoFirstMatch()
-  call inputsave()
-  let userInput = input('')
-  execute "keepjumps normal! gg/".userInput."/\<CR>"
-endfunction
-nnoremap <leader>/f :call GotoFirstMatch()<CR>
-""""""""""""""""""""""""""""""""""""""""""
-
-
-""""""""""""""""""""""""""""""""""""""""""
-" commit message prefix
-function! OnBranch()
-  :/^# On branch /yank
-  :normal P
-  :normal 3dw
-  :normal I(
-  :normal A)
-  :startinsert!
-endfunction
-command! OnBranch call OnBranch()
-""""""""""""""""""""""""""""""""""""""""""
-
-
-""""""""""""""""""""""""""""""""""""""""""
-function! RenderTab()
-:normal iE--------------------
-:normal oB--------------------
-:normal oG--------------------
-:normal oD--------------------
-:normal oA--------------------
-:normal oE--------------------
-endfunction
-command! Tab call RenderTab()
-
-function! RenderBassTab()
-:normal iG--------------------
-:normal oD--------------------
-:normal oA--------------------
-:normal oE--------------------
-endfunction
-command! BTab call RenderBassTab()
-""""""""""""""""""""""""""""""""""""""""""
-
-
-""""""""""""""""""""""""""""""""""""""""""
-" invoke vimgrep with word under cursor
-function! NormalStartVimGrep()
-  let wordUnderCursor = expand("<cword>")
-  call feedkeys(':vimgrep '.wordUnderCursor.' ')
-endfunction
-nnoremap <leader>grep :call NormalStartVimGrep()<cr>
-" invoke vimgrep with visually selected text
-function! VisualStartVimGrep()
-  let selectedText = getline("'<")[getpos("'<")[2]-1:getpos("'>")[2]-1]
-  call feedkeys(':vimgrep '.selectedText.' ')
-endfunction
-vnoremap <leader>grep :call VisualStartVimGrep()<cr>
-""""""""""""""""""""""""""""""""""""""""""
-
-
-""""""""""""""""""""""""""""""""""""""""""
-" misc
-set showtabline=2
-set guioptions-=e
-
-" set t_Co=256
-set laststatus=2
-silent! colorscheme ron
-
-" go to last buffer
-nnoremap <leader>bb :b#<cr>
-
-" quick-fix window always spans windows at bottom
-" https://stackoverflow.com/a/47077341/2295034
-au FileType qf wincmd J
-
-nnoremap <leader>g :Goyo<cr>
-""""""""""""""""""""""""""""""""""""""""""
-
-
-""""""""""""""""""""""""""""""""""""""""""
-function ExecuteFromBuffer()
-  :exe @y
-endfunction
-function! Br() abort
-  normal 0
-  normal mp
-
-  " capture remote in register r
-  normal 0"ryi)
-  " capture branch in register b
-  normal 0f l"by$)
-
-  normal 'p
-  normal o
-
-  " craft checkout statement
-  normal I!git checkout 
-  normal "bp
-  normal a && git reset --hard
-  normal a 
-  normal "rp
-  normal a/
-  normal "bp
-
-  normal "yyy
-  normal "_dd
-  normal 'p
-  silent! call ExecuteFromBuffer()
-  quit!
-endfunction
-
-nmap <silent> <leader>br :call Br()<cr>
-""""""""""""""""""""""""""""""""""""""""""
---]]
 
