49e72734e5c24204c45c25a7b4d2988f3a993462
Jonathan Brink <jonathandavidbrink@gmail.com>
2024-02-13 19:55:46 -0500

    remove oil

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,7 +4,6 @@ if not vim.g.vscode then
     Plug 'junegunn/seoul256.vim'
     Plug 'junegunn/vader.vim'
     Plug 'pocco81/true-zen.nvim'
-    -- Plug 'stevearc/oil.nvim'
     Plug 'tpope/vim-projectionist'
     Plug 'tpope/vim-fugitive'
     Plug 'tpope/vim-flagship'
@@ -47,50 +46,12 @@ if not vim.g.vscode then
     Plug 'numToStr/Comment.nvim'
     vim.call('plug#end');
 
-    -- require("oil").setup({
-    --    columns = {},
-    --    win_options = {
-    --        number = false
-    --    },
-    --    view_options = {
-    --        show_hidden = true
-    --    },
-    --    keymaps = {
-    --        ["ge"] = "actions.open_cmdline"
-    --    }
-    --});
-    --vim.keymap.set(
-    --  "n",
-    --  "-",
-    --  require("oil").open,
-    --  { desc = "Open parent directory" }
-    -- )
-
     vim.keymap.set(
       "n",
       "<leader>z",
       ":TZAtaraxis<CR>",
       {}
     )
-
-    -- require("oil").setup({
-    --    columns = {},
-    --    win_options = {
-    --        number = false
-    --    },
-    --    view_options = {
-    --        show_hidden = true
-    --    },
-    --    keymaps = {
-    --        ["ge"] = "actions.open_cmdline"
-    --    }
-    --});
-    --vim.keymap.set(
-    --  "n",
-    --  "-",
-    --  require("oil").open,
-    --  { desc = "Open parent directory" }
-    --)
 
     vim.keymap.set(
       "n",
