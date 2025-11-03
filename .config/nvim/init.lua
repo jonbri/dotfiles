cea22efc1b87f725c13310ba3bdede86dcd2c709
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-11-03 11:13:33 -0500

    quick-scope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -48,6 +48,7 @@ if not vim.g.vscode then -- in terminal
   Plug 'jonbri/vim-sane'
   Plug 'jonbri/vim-colorpulse'
   Plug 'numToStr/Comment.nvim'
+  Plug 'unblevable/quick-scope'
 
   -- conditionally load copilot plugin
   if vim.fn.getcwd():match("/me$") == nil and ispp ~= "true\n" and (name == "t14\n" or name == "t480s\n" or name == "wsl\n") then Plug 'github/copilot.vim' end
